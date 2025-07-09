cc2149ea9fc8e1019700df3f83fca19978128160
Siddhesh-Ballal <sid1.ballal@gmail.com>
2025-07-09 19:38:06 +0530

    Added Sign in with Google

--- video-app-web-client/app/navbar/navbar.tsx
+++ video-app-web-client/app/navbar/navbar.tsx
@@ -1,12 +1,32 @@
+"use client";
+
 import Image from "next/image";
 import styles from "./navbar.module.css";
 import Link from "next/link";
+import SignIn from "./sign-in";
+import { useEffect, useState } from "react";
+import { onAuthStateChanged, User } from "firebase/auth";
+import { onAuthStateChangedHelper } from "../firebase/firebase";
 export default function Navbar() {
+  // Maintain User auth state in Navbar component itself using react hook: useState.
+  // Initialize user state as null
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChangedHelper((user) => {
+      setUser(user);
+    });
+
+    // Clean up subscription on unmount
+    return () => unsubscribe();
+  });
+
   return (
     <nav className={styles.nav}>
       <Link href="/">
         <Image src="/logo.png" alt="logo" width={180} height={50} />
       </Link>
+      <SignIn user={user} />
     </nav>
   );
 }
